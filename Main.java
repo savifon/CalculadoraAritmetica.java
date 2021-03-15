597e18526d21c22331b5e2a3e1df9f9abb080d82
Savio Fonseca <savifon1@gmail.com>
2021-03-15 12:17:13 +0000

    iniciando ex. lista 1 AED 2

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-/*Faça um programa no qual o usuário possa escolher uma das opções:
+/* Faça um programa no qual o usuário possa escolher uma das opções:
 “1 - Multiplicação de inteiros”,
 “2 - Divisão de inteiros”,
 “3 - Adição de inteiros”,
@@ -8,12 +8,12 @@ Caso o usuário digite 2, ele poderá entrar com dois números.
 O resultado da operação matemática entre eles deverá ser exibido na tela e, logo em
 seguida, retornar ao menu principal.
 Para o caso de divisão por 0, apresentar a devida mensagem de erro para o usuário.
-Nome do programa: CalculadoraAritmetica.java*/
+Nome do programa: CalculadoraAritmetica.java */
 
 class Main {
   public static void main(String[] args) {
     
-		
+
 		
   }
 }
