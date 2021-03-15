13e1d1cd516e3d5421a6c24a62cd627148d10153
Savio Fonseca <savifon1@gmail.com>
2021-03-15 12:15:54 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+/*Faça um programa no qual o usuário possa escolher uma das opções:
+“1 - Multiplicação de inteiros”,
+“2 - Divisão de inteiros”,
+“3 - Adição de inteiros”,
+“4 - Subtração de inteiros” e
+“5 – Sair do programa”.
+Caso o usuário digite 2, ele poderá entrar com dois números.
+O resultado da operação matemática entre eles deverá ser exibido na tela e, logo em
+seguida, retornar ao menu principal.
+Para o caso de divisão por 0, apresentar a devida mensagem de erro para o usuário.
+Nome do programa: CalculadoraAritmetica.java*/
+
+class Main {
+  public static void main(String[] args) {
+    
+		
+		
+  }
+}
